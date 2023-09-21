0224f0c0191cf99f3e2585eb5aba18b36e028944
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-09-21 18:17:13 -0400

    python change 4

--- main.ts
+++ main.ts
@@ -6,15 +6,15 @@
 
 /// This program runs a 6 sided dice  
 */
-  
+
 //  our variable for a random number
 let randomNumber: number
-  
+
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
-  
+
 input.onButtonPressed(Button.A, function () {
-  
+
 randomNumber = randint(1, 6)
 basic.showNumber(randomNumber)
 })
