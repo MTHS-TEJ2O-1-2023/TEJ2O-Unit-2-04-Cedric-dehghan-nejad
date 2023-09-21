f9373a2f1baa02be28c6620ac668917df9ec51b9
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-09-21 18:45:46 -0400

    python changes 8

--- main.ts
+++ main.ts
@@ -4,18 +4,17 @@
  * Created on: Sep 2020
  * This program ...
 
-/// This program runs a 6 sided dice  
+/// This program runs a 6 sided dice
 */
-    
+
 //  our variable for a random number
 let randomNumber: number
-    
+
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
-    
-input.onButtonPressed(Button.A, function () {
-    
-randomNumber = randint(1, 6)
-basic.showNumber(randomNumber)
-})
-    
+
+input.onButtonPressed(Button.A, function () 
+{
+randomNumber = randint(1, 6)  
+basic.showNumber(randomNumber)  
+}) 
