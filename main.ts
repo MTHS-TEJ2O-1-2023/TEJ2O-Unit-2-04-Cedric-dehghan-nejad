d35c03a599dbca02b77ea9aebff702b49c673ff2
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-09-22 10:13:34 -0400

    Temperature change 1

--- main.ts
+++ main.ts
@@ -8,12 +8,12 @@
 */
 
 //  our variable for a random number
-let randomNumber: number
+let temperature: number
 
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
-  randomNumber = randint(1, 6)
-  basic.showNumber(randomNumber)
+  temperature = input.temperature()
+  basic.showNumber(temperature)
 })
